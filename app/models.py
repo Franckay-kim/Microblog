c031daa02e807f009093e82fa1ecd1f5a8519a57
Franckay-kim <fkim6322@gmail.com>
2023-10-31 22:55:33 +0300

    user registration

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('post', backref='author', lazy='dynamic')
+    # posts = db.relationship('post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -32,6 +32,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref="posts", lazy=True)
 
     def __repr__(self):
         return  '<Post {}>'.format(self.body)
