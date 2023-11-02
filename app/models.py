b295c2658018faae5e30eaa7d4b8a4944c21b3ca
Franckay-kim <fkim6322@gmail.com>
2023-11-02 22:21:33 +0300

    defining followers

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 @login.user_loader
 def load_user(id):
@@ -19,6 +24,11 @@ class User(UserMixin, db.Model):
     # posts = db.relationship('post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -44,4 +54,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return  '<Post {}>'.format(self.body)
-
