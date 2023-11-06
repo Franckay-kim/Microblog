7eff1c1fb3ce5a5cf8c6a3198d3e8eb4264110b5
Franckay-kim <fkim6322@gmail.com>
2023-11-06 23:43:27 +0300

    Displaying posts, Explore page

--- app/models.py
+++ app/models.py
@@ -71,7 +71,8 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref="posts", lazy=True)
+    # user = db.relationship('User', backref="posts", lazy=True)
+    author = db.relationship('User', backref="posts", lazy=True)
 
     def __repr__(self):
         return  '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -25,21 +25,13 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, user=current_user)
+        post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    # Displaying posts in homepage
+    posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home Page', form=form,
                            posts=posts)
 
@@ -152,3 +144,12 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+# Explore view function
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return  render_template('index.html', title='Explore', posts=posts)
+
