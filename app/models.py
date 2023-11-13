4f1592b76b31cf79e36a110e57bafdf983818d3a
Franckay-kim <fkim6322@gmail.com>
2023-11-13 22:24:22 +0300

    tests, pagination

--- app/models.py
+++ app/models.py
@@ -65,6 +65,11 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+# The user's own posts
+    def own_posts(self):
+        own = Post.query.filter_by(user_id=self.id)
+        return own.order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
